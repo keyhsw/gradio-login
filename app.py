e90dd63c1c492f894daad591625cf6584df302cc
key <30455795+keyhsw@users.noreply.github.com>
2024-04-11 18:38:41 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,33 @@
+import gradio as gr
+import random
+import time
+
+def authenticate(username, password):
+    if username == USERNAME and password == PASSWORD:
+        return True
+        
+    else:
+        return False
+
+def logout(request: gr.Request):
+
+    print("logged out")
+
+with gr.Blocks() as demo:
+    chatbot = gr.Chatbot()
+    msg = gr.Textbox()
+    clear = gr.ClearButton([msg, chatbot])
+    logout_button = gr.Button(value = "Logout")
+    logout_button.click(logout)
+    # logout_button = gr.LogoutButton()
+    def respond(message, chat_history):
+        bot_message = random.choice(["How are you?", "I'm very hungry"])
+        chat_history.append((message, bot_message))
+        time.sleep(2)
+        return "", chat_history
+
+    msg.submit(respond, [msg, chatbot], [msg, chatbot])
+
+
+if __name__ == "__main__":
+    demo.launch(auth=authenticate,share=True)
